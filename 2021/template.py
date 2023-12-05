5c84992d0cb27dd386bb575f20d1ab10d7467a51
Rodrigo Toste Gomes <rodrigo@memsql.com>
2023-12-04 19:13:32 -0800

    Template for 2021

---
+++ 2021/template.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Day $d of advent of code 2021
+"""
+
+def part1(fname):
+    """
+    Part 1 of the problem.
+    """
+    with open(fname, encoding="utf-8") as f:
+        pass
+
+assert part1("examples/$d1.txt") == 0
+print(part1("input/$d.txt"))
+
+def part2(fname):
+    """
+    Part 2 of the problem.
+    """
+    with open(fname, encoding="utf-8") as f:
+        pass
+
+assert part2("examples/$d1.txt") == 0
+print(part2("input/$d.txt"))
